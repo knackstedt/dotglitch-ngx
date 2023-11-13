c241ce6211c7f4fff4c60f1770b92de24dc038ba
Andrew G. Knackstedt <knackstedt@outlook.com>
2023-11-13 10:48:13 -0500

    ⚙chore(theme) cleanup document.body class management

--- packages/common/src/services/theme.service.ts
+++ packages/common/src/services/theme.service.ts
@@ -3,18 +3,24 @@ import { BehaviorSubject } from 'rxjs';
 
 type AppTheme = "light" | "dark";
 
+const isLightMode = document.body.classList.contains("light");
+
 @Injectable({
     providedIn: 'root'
 })
 export class ThemeService extends BehaviorSubject<AppTheme>{
     constructor() {
-        super("dark");
+        super(isLightMode ? "light" : "dark");
 
         this.subscribe(t => {
-            document.body.classList.remove("dark");
-            document.body.classList.remove("light");
+
+            if (document.body.classList.contains("dark") && t == "light")
+                document.body.classList.remove("dark");
+            if (document.body.classList.contains("light") && t == "dark")
+                document.body.classList.remove("light");
+
             document.body.classList.add(t);
-        })
+        });
     }
 
     public setTheme(t: AppTheme) {
