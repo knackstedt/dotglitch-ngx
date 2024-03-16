0a6fabbba77f867b8d44b1e98b5654c1d9a40049
Andrew G. Knackstedt <knackstedt@outlook.com>
2024-03-16 11:12:05 -0400

    🚀feat(react-magic-wrapper) add Angular magic wrapper method

--- packages/common/src/components/react-magic-wrapper/react-magic-wrapper.component.ts
+++ packages/common/src/components/react-magic-wrapper/react-magic-wrapper.component.ts
@@ -1,8 +1,10 @@
-import { AfterViewInit, Component, OnChanges, OnDestroy, SimpleChanges, ViewContainerRef } from '@angular/core';
+import { AfterViewInit, ApplicationRef, Component, ComponentFactoryResolver, EnvironmentInjector, EventEmitter, Injector, OnChanges, OnDestroy, SimpleChanges, Type, ViewContainerRef, ViewRef, createComponent } from '@angular/core';
 import * as React from 'react';
 import { createRoot, Root } from 'react-dom/client';
 
 import { ThemeService } from '../../services/theme.service';
+import { ulid } from 'ulidx';
+import { Subscription } from 'rxjs';
 
 /**
  * Extend this component to automatically generate
@@ -18,6 +20,85 @@ import { ThemeService } from '../../services/theme.service';
     standalone: true
 })
 export class ReactMagicWrapperComponent implements OnChanges, OnDestroy, AfterViewInit {
+
+    /**
+     * Wrap an angular component inside of a React memo object.
+     * Will attempt to bind @Input and @Output properties if provided,
+     * and will bind the react arguments directly as @Input properties.
+     *
+     * @experimental
+     * @param componentClass Angular component
+     * @param envInjector    An `EnvironmentInjector` instance to be used for the component
+     * @param injector       An `ElementInjector` instance
+     * @param _inputs
+     * @param _outputs
+     * @returns
+     */
+    static WrapAngularComponent = (
+        componentClass: Type<any>,
+        appRef: Omit<ApplicationRef, '_runningTick'>,
+        injector: Injector,
+        _inputs: { [key: string]: any } = {},
+        _outputs: { [key: string]: Function } = {}
+    ) => React.memo((args) => {
+
+        const id = ulid();
+        React.useEffect(() => {
+            const component = createComponent(componentClass, {
+                environmentInjector: appRef.injector,
+                elementInjector: injector,
+                hostElement: document.getElementById(id)
+            });
+
+            appRef.attachView(component.hostView);
+            // @ts-ignore
+            // component.hostView = hostView;
+
+            Object.assign(_inputs, args);
+
+            const { inputs, outputs } = componentClass['ɵcmp'];
+
+            // Returns a list of entries that need to be set
+            // This makes it so that unnecessary setters are not invoked.
+            const updated = Object.entries(inputs).filter(([parentKey, childKey]: [string, string]) => {
+                return component.instance[childKey] != _inputs[parentKey];
+            });
+
+            updated.forEach(([parentKey, childKey]: [string, string]) => {
+                if (_inputs.hasOwnProperty(parentKey))
+                    component.instance[childKey] = _inputs[parentKey];
+            });
+
+            const outputSubscriptions: { [key: string]: Subscription } = {};
+            // Get a list of unregistered outputs
+            const newOutputs = Object.entries(outputs).filter(([parentKey, childKey]: [string, string]) => {
+                return !outputSubscriptions[parentKey];
+            });
+
+            // Reverse bind via subscription
+            newOutputs.forEach(([parentKey, childKey]: [string, string]) => {
+                if (_outputs.hasOwnProperty(parentKey)) {
+                    const target: EventEmitter<unknown> = component.instance[childKey];
+                    const outputs = _outputs;
+
+                    const sub = target.subscribe(outputs[parentKey]); // Subscription
+
+                    outputSubscriptions[parentKey] = sub;
+                }
+            });
+
+            // Wrap the destroy method to safely release the subscriptions
+            const originalDestroy = component.onDestroy?.bind(component);
+            component.onDestroy = (cb) => {
+                Object.values(outputSubscriptions).forEach(s => s.unsubscribe());
+                originalDestroy?.(cb);
+            }
+
+            component.changeDetectorRef.detectChanges();
+        }, []);
+
+        return React.createElement("div", { id });
+    });
 
     /**
      * The react component to be wrapped.
